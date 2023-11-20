a06dff1198d607756bbffa999223e6b45b30c113
HectorMC02 <hecmataca@gmail.com>
2023-11-20 10:25:33 +0100

    Deleting innecesary comments and modifying the readme file

--- ProgressBar/README.md
+++ ProgressBar/README.md
@@ -1,7 +1,7 @@
 #Progress Bar Script
-This script allows you to set a specific time passed as a parameter for a
-little bar which progresses during that time.
+This script allows you to set a specific time passed as a parameter for a little bar which progresses during that time.
 You must create a script doing something you want, then call this one.
 The usage is, inside your own script:
 	source progress_bar.sh <amount_of_time>
+Both .sh files must be in the same folder (your script and progress_bar.sh script)
 It does not need any dependences.
--- ProgressBar/progress_bar.sh
+++ ProgressBar/progress_bar.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
 function progress_bar {
-	#Para hacer el porcentaje tomar el tiempo total ($1) siendo eso el 100%, hacer una regla de 3 avanzando a medida que baje el tiempo
 	if (( $(echo "$# == 1" | bc) ))
 	then
 	if (( $(echo "$1 > 0" | bc) ))
 	then
-		default=60 #cantidad por defecto del tamaño de la barra de carga
-		sym='' #cantidad completada
-		empt='-' #cantidad no completada
+		default=60
+		sym=''
+		empt='-'
 		temp=0
-		bpers=$(echo "scale=2; $default/$1" | bc) #blocks per second
-		empt=$(printf -- '-%.0s' $(seq 1 $default)) #relleno empt con todos los "vacios"
+		bpers=$(echo "scale=2; $default/$1" | bc)
+		empt=$(printf -- '-%.0s' $(seq 1 $default))
 		summod=0
 		clear=$(echo "\033[1K\c")
 		for (( i=0 ; i<=$1; i++ )) do
 			perc="$(echo "scale=2; $temp*100/$1" | bc)% "
-			symexactos=$(echo "scale=0; $bpers/1" | bc) #calculo de los simbolos exactos (si 2 --> 2/5 --> 2.5 --> symexactos = 2)
+			symexactos=$(echo "scale=0; $bpers/1" | bc)
 			if (( $(echo "$symexactos == 0" | bc) ))
 			then
 				mod=$(echo "$bpers % 1" | bc)
-			else	
-				mod=$(echo "$bpers % $symexactos" | bc) #sobrante de symexactos, para el caso anterior 0.5
+			else
+				mod=$(echo "$bpers % $symexactos" | bc)
 			fi
 			if (( $(echo "$i == 0" | bc) ))
 			then
 				echo -e $clear
-				echo -ne "\r$perc$sym$empt $(($1-$temp)) secs" #no avanza debido a que sym esta vacio
+				echo -ne "\r$perc$sym$empt $(($1-$temp)) secs"
 			else
 				if (( $(echo "$symexactos > 0" | bc) ))
 				then
-					sym=$sym$(printf -- '#%.0s' $(seq 1 $symexactos)) #ponemos la cantidad de sym que tocan
-					empt=$(echo ${empt%$(printf -- '?%.0s' $(seq 1 $symexactos))}) #ponemos los "vacios" que toquen
+					sym=$sym$(printf -- '#%.0s' $(seq 1 $symexactos))
+					empt=$(echo ${empt%$(printf -- '?%.0s' $(seq 1 $symexactos))})
 				fi
-				summod=$(echo "$summod+$mod" | bc) #suma de los mod que sobran
+				summod=$(echo "$summod+$mod" | bc)
 				if (( $(echo "$summod >= 1"| bc) ))
 				then
 					sym=$sym#
 					empt=$(echo ${empt%?})
-					summod=$(echo "$summod-1" | bc) #quitamos uno para mostrarlo
+					summod=$(echo "$summod-1" | bc)
 					echo -e $clear
 					echo -ne "\r$perc$sym$empt $(($1-$temp)) secs"
 				else
@@ -46,7 +45,7 @@ function progress_bar {
 					then
 						sym=$sym#
 						empt=$(echo ${empt%?})
-						summod=0 #vaciamos summod
+						summod=0
 					echo -e $clear
 						echo -ne "\r$perc$sym$empt $(($1-$temp)) secs"
 					fi
