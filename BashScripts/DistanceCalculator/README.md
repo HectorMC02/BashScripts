206018e0372c2c1876a79507ccfb2f6c7ea57623
HectorMC02 <hecmataca@gmail.com>
2025-03-08 00:53:12 +0100

    Start repository for distance calculator

---
+++ BashScripts/DistanceCalculator/README.md
@@ -0,0 +1,46 @@
+
+## About
+
+> **[?]**
+> This script allows you to set a specific time passed as a parameter for a bar which progresses during that time.
+
+<details open>
+<summary>Screenshots</summary>
+<br>
+
+|                          Execution of the script                      |
+| :-------------------------------------------------------------------: |
+| <img src="docs/images/distance_exec.gif" title="distance_exec" width="100%">|
+
+</details>
+
+### Built With
+
+> **[?]**
+> This is scripted on Bash lenguage, no dependences apart from having jq installed
+
+### Getting Started
+> This script is for showing use of APIs
+
+### Prerequisites
+
+> **[?]**
+> No prerrequisites needed
+
+### Installation
+
+> **[?]**
+> No instalation required
+
+## Usage
+
+> **[?]**
+> You have to call the script from the terminal and write 2 cities
+> To execute:
+
+        ./distanceCalc.sh
+
+## Authors & contributors
+
+The original setup of this repository is by [Hector Matanza Cavero](https://github.com/HectorMC02).
+
---
+++ BashScripts/DistanceCalculator/distanceCalc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Set the origin city"
+read origin
+wget -O ./.origintext.json -q "https://api-v2.distancematrix.ai/maps/api/geocode/json?address=$origin&key=ZwdrZdC61O7hu1inldCE7wdkfUVfZIAKw3PyRtjO4xLS6Z6mtTArSiHpLsxTetGH"
+origintocoordinates=$(echo $(cat .origintext.json | jq -r ".result[] | (.geometry.location.lat)"),$(cat .origintext.json | jq -r ".result[] | (.geometry.location.lng)"))
+
+echo "Set the destination city"
+read destin
+wget -O ./.destintext.json -q "https://api-v2.distancematrix.ai/maps/api/geocode/json?address=$destin&key=ZwdrZdC61O7hu1inldCE7wdkfUVfZIAKw3PyRtjO4xLS6Z6mtTArSiHpLsxTetGH"
+destintocoordinates=$(echo $(cat .destintext.json | jq -r ".result[] | (.geometry.location.lat)"),$(cat .destintext.json | jq -r ".result[] | (.geometry.location.lng)"))
+
+wget -O ./.distance.json -q "https://api.distancematrix.ai/maps/api/distancematrix/json?origins=$origintocoordinates&destinations=$destintocoordinates&key=uHQ09Fwvl6UIMCSta9TkPjPyeso2d674TJxIKo8HcSSOUG8Z3b64dNsEA5H28Wun"
+distance=$(cat .distance.json | jq -r ".rows[].elements[].distance | (.text)" | grep -Eo "[0-9]+.[0-9]+")
+if [[ -z $distance ]]
+then
+	echo "Can't take the distance. Can you go one to another by car?"
+else
+	distancemiles=$(echo "scale=2;"$distance*1.6093 | bc)
+	echo "The distance between $origin and $destin is approximately $distance km or $distancemiles miles"
+fi
+rm -f .origintext.json
+rm -f .destintext.json
+rm -f .distance.json
